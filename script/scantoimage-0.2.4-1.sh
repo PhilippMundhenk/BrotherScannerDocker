bb3a33dea3322f470d90bc17269dc09eef2363d7
Philipp Mundhenk <philipp@mundhenk.org>
2018-08-15 17:32:30 +0200

    added front and rear page scanning and automatic merging

--- script/scantoimage-0.2.4-1.sh
+++ script/scantoimage-0.2.4-1.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 # $1 = scanner device
 # $2 = friendly name
 
@@ -10,7 +10,8 @@ export $(grep -v '^#' /opt/brother/scanner/env.txt | xargs)
 resolution=300
 device=$1
 date=$(date +%Y-%m-%d-%H-%M-%S)
-output_file=/scans/$date"-page%d.pnm"
+filename_base=/scans/$date"-page"
+output_file=$filename_base"%d.pnm"
 
 if [ "`which usleep  2>/dev/null `" != '' ];then
     usleep 100000
@@ -18,7 +19,7 @@ else
     sleep  0.1
 fi
 scanadf --device-name "$device" --source "Automatic Document Feeder(centrally aligned)" --resolution $resolution -o $output_file
-if [ ! -s $output_file ];then
+if [ ! -s $filename_base"1.pnm" ];then
   if [ "`which usleep  2>/dev/null `" != '' ];then
     usleep 1000000
   else
@@ -27,13 +28,7 @@ if [ ! -s $output_file ];then
   scanadf --device-name "$device" --source "Automatic Document Feeder(centrally aligned)" --resolution $resolution -o $output_file
 fi
 
-gm convert /scans/$date-page*.pnm /scans/$date.jpg
+gm convert /scans/$date-page*.pnm /scans/$date.pdf
 rm /scans/$date-page*.pnm
 
-if [[ -z "${SSH_USER}" ]]; then
-  /opt/brother/scanner/brscan-skey/script/trigger_inotify.sh $SSH_USER $SSH_PASSWORD $SSH_HOST $SSH_PATH $date.pdf
-fi
-
-if [[ -z "${FTP_USER}" ]]; then
-  curl --ftp-ssl -T "/scans/$date.pdf" -k -u "$FTP_USER:$FTP_PASSWORD" "ftp://$FTP_HOST/$FTP_PATH"
-fi
+/opt/brother/scanner/brscan-skey/script/trigger_inotify.sh $SSH_USER $SSH_PASSWORD $SSH_HOST $SSH_PATH $date.pdf
