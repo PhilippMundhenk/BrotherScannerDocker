bfa8264b3c3c3c5c4cff58151da665a908466a49
Chris Wiggins <me@cwiggs.com>
2023-08-01 16:35:48 -0700

    fix: forgot to cd first

--- script/sendtoftps.sh
+++ script/sendtoftps.sh
@@ -4,6 +4,8 @@ address=$3
 filepath=$4
 file=$5
 
+cd /scans
+
 if [ -z "${user}" ] || [ -z "${password}" ] || [ -z "${address}" ] || [ -z "${filepath}" ] || [ -z "${file}" ]; then
   echo "FTP environment variables not set, skipping inotify trigger."
 else
