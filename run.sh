9f7d07b226aca6f20f2d644d3435ada9ca1e5e89
Philipp Mundhenk <philipp@mundhenk.org>
2023-08-03 21:56:15 +0200

    Update run.sh
    quoted docker volumes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker run \
     -d \
-    -v /home/$USER/scans:/scans \
-    -v $PWD/script:/opt/brother/scanner/brscan-skey/script/ \
+    -v "/home/$USER/scans:/scans" \
+    -v "$PWD/script:/opt/brother/scanner/brscan-skey/script/" \
     -e NAME="Scanner" \
     -e MODEL="MFC-L2700DW" \
     -e IPADDRESS="10.0.0.1" \
