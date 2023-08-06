6cd9609ab5a71036cbd4e889121703cdc5157437
Philipp Mundhenk <philipp@mundhenk.org>
2023-08-06 15:37:10 +0200

    added logging output to stdout

--- files/runScanner.sh
+++ files/runScanner.sh
@@ -1,6 +1,8 @@
 adduser $USERNAME --disabled-password --force-badname --gecos ""
 mkdir -p /scans
 chmod 777 /scans
+touch /var/log/scanner.log
+chown $USERNAME /var/log/scanner.log
 env > /opt/brother/scanner/env.txt
 chmod -R 777 /opt/brother
 su - $USERNAME -c "/usr/bin/brsaneconfig4 -a name=$NAME model=$MODEL ip=$IPADDRESS"
@@ -8,6 +10,6 @@ su - $USERNAME -c "/usr/bin/brscan-skey"
 echo "startup successful"
 while true;
 do
-  sleep 1000
+  tail -f /var/log/scanner.log
 done
 exit 0
