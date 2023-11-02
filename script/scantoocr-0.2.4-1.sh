79f2e46b7affd8b30cc019fd961bad19978de04c
Philipp Mundhenk <philipp@mundhenk.org>
2023-11-02 17:31:35 +0100

    removed unmaintained script content

--- script/scantoocr-0.2.4-1.sh
+++ script/scantoocr-0.2.4-1.sh
@@ -3,42 +3,10 @@
 # $2 = friendly name
 
 {
-#override environment, as brscan is screwing it up:
-export $(grep -v '^#' /opt/brother/scanner/env.txt | xargs)
 
-# Resolution (dpi):
-# 100,200,300,400,600
-resolution=300
-device=$1
-date=$(date +%Y-%m-%d-%H-%M-%S)
-filename_base=/scans/$date"-page"
-output_file=$filename_base"%d.pnm"
-
-if [ "`which usleep  2>/dev/null `" != '' ];then
-    usleep 100000
-else
-    sleep  0.1
-fi
-scanadf --device-name "$device" --source "Automatic Document Feeder(centrally aligned)" --resolution $resolution -o $output_file
-if [ ! -s $filename_base"1.pnm" ];then
-  if [ "`which usleep  2>/dev/null `" != '' ];then
-    usleep 1000000
-  else
-    sleep  1
-  fi
-  scanadf --device-name "$device" --source "Automatic Document Feeder(centrally aligned)" --resolution $resolution -o $output_file
-fi
-
-gm convert /scans/$date-page*.pnm /scans/$date.pdf
-rm /scans/$date-page*.pnm
-
-/opt/brother/scanner/brscan-skey/script/trigger_inotify.sh "${SSH_USER}" "${SSH_PASSWORD}" "${SSH_HOST}" "${SSH_PATH}" "${date}.pdf"
-
-/opt/brother/scanner/brscan-skey/script/sendtoftps.sh \
-  "${FTP_USER}" \
-  "${FTP_PASSWORD}" \
-  "${FTP_HOST}" \
-  "${FTP_PATH}" \
-  "${date}.pdf"
+echo "ERROR!"
+echo "This function is not implemented."
+echo "You may implement your own script and mount under $0."
+echo "Check out scripts in same folder or https://github.com/PhilippMundhenk/BrotherScannerDocker for examples."
 
 } >> /var/log/scanner.log 2>&1
