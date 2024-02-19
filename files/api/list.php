bfad1cd28dca389f7d20383ab3b230fb6eea54ad
philippderdiedas <56478008+philippderdiedas@users.noreply.github.com>
2024-02-19 02:42:07 +0100

    remove . and .. from list.php
    followup to previous commit

--- files/api/list.php
+++ files/api/list.php
@@ -1,11 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $files = scandir("/scans", SCANDIR_SORT_DESCENDING);
-        if(array_key_exists("num", $_GET)) {
-                $num = $_GET["num"];
-        } else {
-                $num = count($files);
-        }
+        $files = array_diff(scandir("/scans", SCANDIR_SORT_DESCENDING), array('..', '.'));
+        $num = $_GET["num"] ?? count($files);
         for ($i = 0; $i < $num; $i++) {
                 echo $files[$i]."<br>";
         }
