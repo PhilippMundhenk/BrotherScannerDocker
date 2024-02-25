57c59894ef0bf623b83eb742f51fd1288168df0d
Philipp Mundhenk <philipp@mundhenk.org>
2024-02-25 14:17:46 +0100

    removed hint to filename

--- files/api/download.php
+++ files/api/download.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                         readfile($filepath);
                 } else {
                         http_response_code(404);
-                        die("Error: File does not exist! Path: $filepath");
+                        die("Error: File does not exist!");
                 }
         } else {
                 http_response_code(400);
