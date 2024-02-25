3f98ee3b14994cacf8579065dc6ea73e4369094f
Philipp Mundhenk <philipp@mundhenk.org>
2024-02-25 13:27:53 +0100

    account for ping being disabled

--- files/api/status.php
+++ files/api/status.php
@@ -1,11 +1,16 @@
 <?php
 include_once(__DIR__."/lib/config.php");
 
+$status_file=__DIR__ . '/reachable.txt'
+
 exec("ps aux | grep '[s]canimage'", $output, $retVal);
 if(!empty($output)) {
     echo("scanning");
 } else {
-    if (str_contains(file_get_contents(__DIR__ . '/reachable.txt'), '1')) {
+	if (!file_exists($status_file)) {
+		echo("unknown");
+	}
+    if (str_contains(file_get_contents($status_file), '1')) {
         echo("online");
     } else {
         echo("offline");
