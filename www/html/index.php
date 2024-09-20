75383e331e10b4982a279e6c7e2a988204e82978
Philipp <philipp@freilinger.org>
2024-09-20 20:14:32 +0200

    added routes

--- www/html/index.php
+++ www/html/index.php
@@ -11,62 +11,98 @@ require_once('helper.php');
 if (!isset($TZ)) {
 	$TZ = 'Europe/Berlin';
 }
-
 date_default_timezone_set($TZ);
 
-session_start();
+#session_start();
 
 $router = new AltoRouter();
 $router->addMatchTypes(array('char' => '(?:[^\/]*)'));
 
 
+// Frontend routes
+
 $router->map( 'GET', '/', function() {
-	require 'views/frontend/home.php';
+	require_once 'views/frontend/home.php';
 });
 
 
 $router->map( 'GET', '/list-files', function() {
-	require 'views/frontend/list.php';
+	require_once 'views/frontend/file-list.php';
 });
 
 
+$router->map( 'GET', '/file/[char:file]/rename', function( $file) {
+	require_once 'views/frontend/file-rename.php';
+});
+
+
+$router->map( 'GET', '/file/[char:file]/delete', function( $file) {
+	require_once 'views/frontend/file-delete.php';
+});
+
+
+// API routes
+
 $router->map( 'GET', '/api/scanner/status', function() {
-	require 'views/api/scanner-status.php';
+	require_once 'views/api/scanner-status.php';
 });
 
 
 $router->map( 'POST', '/api/scanner/scanto', function() {
     $scanto = $_POST["target"];
     $method = 'return';
-	require 'views/api/scanner-scanto.php';
+	require_once 'views/api/scanner-scanto.php';
 });
 
 
 $router->map( 'GET', '/api/scanner/scanto/[char:parameter]', function( $parameter) {
     $scanto = $parameter;
     $method = 'wait';
-	require 'views/api/scanner-scanto.php';
+	require_once 'views/api/scanner-scanto.php';
+});
+
+
+$router->map( 'GET', '/api/file-list', function() {
+	require_once 'views/api/file-list.php';
+});
+
+$router->map( 'GET', '/api/file/[char:file]/info', function( $file) {
+	require_once 'views/api/file-info.php';
 });
 
 
 $router->map( 'GET', '/api/file/[char:file]/download', function( $file) {
-	require 'views/api/file-download.php';
+	require_once 'views/api/file-download.php';
+});
+
+$router->map( 'DELETE', '/api/file/[char:file]/delete', function( $file) {
+	require_once 'views/api/file-delete.php';
+});
+
+
+$router->map( 'PUT', '/api/file/[char:file]/rename', function( $file) {
+	require_once 'views/api/file-rename.php';
 });
 
 
 $router->map( 'GET', '/api/dev/timezone', function() {
-	require 'views/api/dev-timezone.php';
+	require_once 'views/api/dev-timezone.php';
 });
 
 
 $match = $router->match();
 
 
+// Call closure or throw 404 status if route not found
+
 if( is_array($match) && is_callable( $match['target'] ) ) {
-	call_user_func_array( $match['target'], $match['params'] ); 
+    call_user_func_array( $match['target'], $match['params'] );
 } else {
-    send_error_page(404, 'Not Found', 'The requested resource could not be found.');
-
+    if (str_contains($_SERVER['REQUEST_URI'], '/api')) {
+        send_json_error(404, 'Not Found');
+    } else {
+        send_error_page(404, $page_title='404', $page_message='Sorry, the page you are looking for could not be found.');
+    }
+    exit();
 }
-
 ?>
