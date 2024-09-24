7d9a553be600f0314663faa6f75c6bbe5fc92a37
Philipp <philipp@freilinger.org>
2024-09-24 13:38:50 +0200

    changed my mind about this. removed bash in do it in php

--- www/private/views/api/file-rename.php
+++ www/private/views/api/file-rename.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 include('config.php');
 require_once('helper.php');
 
@@ -16,6 +12,15 @@ if(!isset($file)) {
         send_json_error(400, "No file specified");
 }
 
+
+// Function to get file access and modification times
+function getFileTimes($original_filename) {
+    return [
+        'access_time' => fileatime($original_filename), // Access time
+        'modification_time' => filemtime($original_filename) // Modification time
+    ];
+}
+
 $file_info = file_get_verified_fileinfo('/scans/', urldecode($file));
 
 $original_filename = $file_info['full_path'];
@@ -26,7 +31,6 @@ $data = json_decode($jsonData, true);
 
 
 if ($data !== null) {
-   // Access the data and perform operations
    $new_filename = $data['new_filename'];
    $new_filename_prefix = $data['new_filename_prefix'];
    
@@ -51,19 +55,20 @@ if (file_is_valid_name_string($new_filename)){
         $final_filename = $file_info['dir'] . '/' . $file_info['date_from_file'] . '-' . $file_info['time_from_file'] . ' ' . $target_filename;
     }
 
-    
-    $UID = escapeshellarg($UID);  // Ensure UID is safe
-    $original_filename = escapeshellarg($original_filename);  // Escape the filename for shell
-    $final_filename = escapeshellarg($final_filename);        // Escape the new filename
 
-    #$command = 'sudo -u \#'.$UID.' mv '.$original_filename.' '.$final_filename;
+    // Get access and modification times of the old file
+    $times = getFileTimes($original_filename);
 
-    //looks complicated, but preserve the original file creation time
-    #$command = 'sudo -u \#'.$UID.' cp --preserve=all '.$original_filename.' '.$final_filename.' && rm '.$original_filename;
-    $command = 'sudo -u \#'.$UID.' /opt/brother/scanner/brscan-skey/script/rename-preserve-datetime.sh '.$original_filename.' '.$final_filename ;
-    $output = shell_exec($command);
+    // Rename the file
+    if (rename($original_filename, $final_filename)) {
+        
+        // Restore access and modification time using 'touch'
+        @touch($final_filename, $times['modification_time'], $times['access_time']);
+        send_json_error(200, "Renamed file successfully");
+    } else {
+        send_json_error(400, "Error renaming the file");
+    }  
 
-    json(array('status' => 'success'));
 }else{
     send_json_error(400, "Invalid filename");
 }
