d136280a711949307535fbe951face966b759805
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-09-29 11:41:18 +0200

    Completely remove shell scripts

--- script/scantofile.py
+++ script/scantofile.py
@@ -1,1 +1,21 @@
-scantofile-0.2.4-1.py
+#!/usr/bin/python3
+# $1 = scanner device
+# $2 = friendly name
+
+import sys
+
+from scanner import read_environment, scan_front
+
+if __name__ == "__main__":
+    # Open the log file in append mode
+    with open("/var/log/scanner.log", "a") as log:
+        # Redirect stdout to the log file
+        sys.stdout = log
+        sys.stderr = log
+
+        read_environment()
+
+        device = None
+        if len(sys.argv) > 1:
+            device = sys.argv[1]
+        scan_front(log, device)
