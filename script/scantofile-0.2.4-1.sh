039d7488ccb4192219b8f56627d4271c1dbda3e5
James Wilson <drakeapps@users.noreply.github.com>
2025-12-21 11:26:15 -0600

    Adjust scanimage dimensions
    Updated scanimage parameters to use full dimensions.

--- script/scantofile-0.2.4-1.sh
+++ script/scantofile-0.2.4-1.sh
@@ -33,7 +33,7 @@
     # `brother4:net1;dev0` device name gets passed to scanimage, which it refuses as an invalid device name for some reason.
     # Let's use the default scanner for now
     # scanimage -l 0 -t 0 -x 215 -y 297 --device-name="$1" --resolution="$2" --batch="$3"
-    scanimage -l 0 -t 0 -x 215 -y 297 --format=pnm --resolution="$2" --batch="$3"
+    scanimage -l 0 -t 0 -x 0 -y 0 --format=pnm --resolution="$2" --batch="$3"
   }
 
   if [ "$(which usleep 2>/dev/null)" != '' ]; then
